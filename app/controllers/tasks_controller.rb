b172a2c838698641965fbf7b90b2d83a70d2f33b
Camille De Peretti <camilledeperetti10@gmail.com>
2019-08-14 11:08:17 +0200

    xo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,39 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(tasks_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.update(tasks_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
-
-
-rails generate model Task title:string details:text completed:boolean(false)
